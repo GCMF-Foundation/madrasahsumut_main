179911fb35c92c233433eb8fd220d7d23f454a7a
Auzan <auzan.ip@gmail.com>
2022-03-08 09:47:09 +0700

    first commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import './style.css'
+
+
+function Index( props ) {
+    return (
+        <footer className="col-12 footer">
+            <div className="justify-content-center">
+                <div className="col-12 text-light">
+                    <ul className="list-inline d-flex footer-list">
+                        <li>
+                            <a target="_blank" rel="noreferrer" href="https://s.id/geschool" className="footer-link bold">Road Map to Geschool</a>
+                        </li>
+                    </ul>
+                    <ul className="list-inline d-flex footer-list link-list">
+                        <li>
+                            <a target="_blank" rel="noreferrer" href="http://disdik.sumutprov.go.id/" className="footer-link">Dinas Pendidikan Provinsi Sumatera Utara</a>
+                        </li>
+                        <li>
+                            <a target="_blank" rel="noreferrer" href="https://sumut.kemenag.go.id/" className="footer-link">Kemenag Provinsi Sumatera Utara</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="col-12 copyright mt-2">
+                    &copy; {props.copyright}
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Index;
